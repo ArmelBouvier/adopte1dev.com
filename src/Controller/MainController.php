d83e2687287350373ae6118b12eba0b10e79b926
ArmelBouvier <60919177+ArmelBouvier@users.noreply.github.com>
2021-03-04 16:38:22 +0100

    système d'authentification ok

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     /**
-     * @Route("/", name="main")
+     * @Route("/", name="index")
      */
     public function index(): Response
     {
